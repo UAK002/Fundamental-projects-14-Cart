f702362e6b50d182ba5c799335329956d2955d33
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-22 11:34:55 +0530

    274. Cart - UseReducer Setup

--- src/context.jsx
+++ src/context.jsx
@@ -1,11 +1,21 @@
 import { useContext, useReducer, useEffect, createContext } from 'react';
+import reducer from './reducer';
 
 const AppContext = createContext();
 
+const initialState = {
+  loading: false,
+  cart: [],
+};
+
 export const AppProvider = ({ children }) => {
-  const greeting = 'Hello there';
+  //   const greeting = 'Hello there';
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <AppContext.Provider value={{ greeting }}>{children}</AppContext.Provider>
+    // <AppContext.Provider value={{ greeting }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
   );
 };
 
